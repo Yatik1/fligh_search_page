da04e191abf6d9d80fecd71ed1074cb8bc763c0f
Yatik1 <yatiksrivastava1@gmail.com>
2024-10-04 17:31:04 +0530

    button debug

--- components/SearchBox.tsx
+++ components/SearchBox.tsx
@@ -6,15 +6,15 @@ import { Button } from './ui/button'
 
 const SearchBox = () => {
   return (
-    <div className='absolute top-[20%] left-[50%] translate-x-[-50%] translate-y-[-50%] border border-[#E6E8EB] p-4 rounded-2xl shadow-lg'>
-      <div className='bg-[#F5F7FA] p-3 rounded-[6px] w-[7.85rem] flex items-center justify-center'>
+    <div className='border border-[#E6E8EB] px-7 py-8 rounded-2xl shadow-lg'>
+      <div className='bg-[#F5F7FA] px-3 py-2 rounded-[6px] w-[7.85rem] flex items-center justify-center'>
          <h2 className='text-[1rem]'>Flights</h2>
       </div>
 
       <div className="flex flex-row items-center justify-between gap-2 mt-[1.5rem]">
         
         <Searchbar label="Where from?" type='select'/>
-        <Icon icon={<ArrowLeftRight/>} />
+        <Icon icon={<ArrowLeftRight/>} bg={true} />
         <Searchbar label="Where to?" type='select'/>
 
         <Searchbar label="Departure" type='date'/>
@@ -23,8 +23,8 @@ const SearchBox = () => {
         
       </div>
       
-     <div className="flex items-center justify-end mt-6">
-       <Button className='flex items-center bg-[#003E39] justify-center'>
+     <div className="flex justify-end mt-6">
+       <Button className='w-[15.6rem] flex items-center bg-[#003E39] justify-center'>
         <Icon icon={<Search/>} />
         Search flights
       </Button>
