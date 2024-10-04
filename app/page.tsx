511df6e550b1f4e114a4a43947216947496fc6eb
Yatik1 <yatiksrivastava1@gmail.com>
2024-10-04 17:30:36 +0530

    page app

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-// import Searchbar from '@/components/Searchbar'
 import SearchBox from '@/components/SearchBox'
 import React from 'react'
 
 export default function Page() {
   return (
-    <div className='relative h-screen'>
-      <h3 className='absolute top-4 left-[50%] translate-x-[-50%] text-[2.25rem] font-light'>Good afternoon, Brian</h3>
+    <div className='flex flex-col items-center justify-start mt-[6.7rem] gap-6'>
+      <h3 className='flex items-center justify-center text-[2.25rem] poppins tracking-tighter'>Good afternoon, Brian</h3>
       <SearchBox />
     </div>
   )
