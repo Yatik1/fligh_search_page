655f2de8c366dc084dd302e1fc91a2838f7a8aae
Yatik1 <yatiksrivastava1@gmail.com>
2024-10-07 20:11:49 +0530

    styles

--- components/SearchNav.tsx
+++ components/SearchNav.tsx
@@ -8,8 +8,8 @@ const SearchNav = () => {
     const {whereFrom,whereTo,departure,returnDate} = useSearchContext() as SearchContextType
 
   return (
-    <div className='px-10  py-7 bg-red-100'>
-        <div className="w-fit flex items-center gap-8 border border-black rounded-full p-4">
+    <div className='px-10  py-7 border-b border-[#E6E8EB]'>
+        <div className="w-fit flex items-center gap-8 border border-[#E6E8EB] rounded-full p-4 text-md tracking-tight">
             <div className="flex items-start justify-center">{whereFrom}</div>
             <div className="flex items-start justify-center">{whereTo}</div>
             <div className="flex items-start justify-center">{departure}</div>
