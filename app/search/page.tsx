50fdad8e8f1f3047b04f8a809bc98fda4437ffdf
Yatik1 <yatiksrivastava1@gmail.com>
2024-10-07 19:55:45 +0530

    page tsx

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -5,15 +5,11 @@ import React from 'react'
 
 function page() {
 
-    const {whereFrom,whereTo,departure,returnDate} = useSearchContext() as SearchContextType
+    
 
   return (
     <div>
-        <h1>Search page</h1>
-        <p>Where from: {whereFrom}</p>
-        <p>Where to: {whereTo}</p>
-        <p>Departure: {departure}</p>
-        <p>Return date: {returnDate}</p>    
+        this is search page
     </div>
   )
 }
