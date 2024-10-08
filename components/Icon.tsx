fd3596ab94fbba0002d38079ba7e04a4db08ff40
Yatik1 <yatiksrivastava1@gmail.com>
2024-10-08 20:45:36 +0530

    Icons

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -3,13 +3,15 @@ import React from 'react'
 interface IconProps{
     icon:JSX.Element;
     onclick?:()=>void;
+    className?:string;
     bg?:boolean;
+    border?:boolean;
 }
 
-const Icon = ({icon,onclick,bg}:IconProps) => {
+const Icon = ({icon,onclick,bg,className,border}:IconProps) => {
   return (
-    <div className={`p-[0.9rem] rounded-full flex items-center justify-center ${bg ? "bg-[#F5F7FA]" : ""}`} onClick={onclick}>
-        <div className={`${bg ? "text-black" : "text-[#C9CACC]"} w-5 h-5 flex items-center justify-center`}>
+    <div className={`p-[0.9rem] rounded-full flex items-center justify-center ${bg ? "bg-[#F5F7FA]" : ""} ${border && "border border-[#E6E8EB]"}`} onClick={onclick}>
+        <div className={`${bg ? "text-black" : "text-[#C9CACC]"} w-5 h-5 flex items-center justify-center ${className}`}>
             {icon}
         </div>
     </div>
