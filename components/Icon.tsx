073f6611554b76e51fbe9a6a53031469e7a38667
Yatik1 <yatiksrivastava1@gmail.com>
2024-10-09 23:04:53 +0530

    more dynamic

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -3,15 +3,14 @@ import React from 'react'
 interface IconProps{
     icon:JSX.Element;
     onclick?:()=>void;
-    className?:string;
-    bg?:boolean;
-    border?:boolean;
+    iconStyle?:string;
+    bgStyle?:string;
 }
 
-const Icon = ({icon,onclick,bg,className,border}:IconProps) => {
+const Icon = ({icon,onclick,iconStyle,bgStyle}:IconProps) => {
   return (
-    <div className={`p-[0.9rem] rounded-full flex items-center justify-center ${bg ? "bg-[#F5F7FA]" : ""} ${border && "border border-[#E6E8EB]"}`} onClick={onclick}>
-        <div className={`${bg ? "text-black" : "text-[#C9CACC]"} w-5 h-5 flex items-center justify-center ${className}`}>
+    <div className={bgStyle} onClick={onclick}>
+        <div className={iconStyle}>
             {icon}
         </div>
     </div>
